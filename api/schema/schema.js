2023268e56d60639277fff7d97368ca45233ae94
Tuomas Pöyry <tuomas@munkkeli.io>
2020-04-25 13:36:28 +0300

    GraphQL URL shortening endpoint

--- api/schema/schema.js
+++ api/schema/schema.js
@@ -1,12 +1,75 @@
 'use strict';
+const URL = require('../models/urlModel');
 
 const {
   GraphQLObjectType,
   GraphQLID,
   GraphQLString,
   GraphQLList,
+  GraphQLInt,
   GraphQLSchema,
   GraphQLNonNull,
+  GraphQLBoolean,
 } = require('graphql');
 
-module.exports = new GraphQLSchema({});
+const urlType = new GraphQLObjectType({
+  name: 'url',
+  description: 'Shortened URL record',
+  fields: () => ({
+    _id: { type: GraphQLID },
+    hash: { type: GraphQLString },
+    url: { type: GraphQLString },
+    // TODO: user
+    password: { type: GraphQLString },
+    isObscured: { type: GraphQLBoolean },
+    expiresAt: { type: GraphQLInt },
+    updatedAt: { type: GraphQLInt },
+    createdAt: { type: GraphQLInt },
+  }),
+});
+
+const Mutation = new GraphQLObjectType({
+  name: 'MutationType',
+  description: 'Mutations...',
+  fields: {
+    shortenUrl: {
+      type: urlType,
+      description: 'Shorten a URL',
+      args: {
+        url: { type: new GraphQLNonNull(GraphQLString) },
+        password: { type: GraphQLString },
+        isObscured: { type: GraphQLBoolean },
+        expiresAt: { type: GraphQLInt },
+      },
+      resolve(parent, args, { req, res, next, checkAuth }) {
+        // TODO: Make auth work
+        // checkAuth(req, res, next);
+
+        return URL.create({
+          url: args.url,
+          password: args.password,
+          isObscured: args.isObscured === true,
+          expiresAt: args.expiresAt,
+        });
+      },
+    },
+  },
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    statistics: {
+      type: GraphQLInt,
+      description: 'Dummy endpoint for now',
+      resolve(parent, args) {
+        return 1;
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+  mutation: Mutation,
+});
