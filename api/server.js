b8e173e9035f0c5b3c797af4ed193e3eee2aeaa5
Tuomas Pöyry <tuomas@munkkeli.io>
2020-04-27 15:57:35 +0300

    URL shortener is now functional!

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const express = require('express');
 const passport = require('passport');
 const helmet = require('helmet');
+const cors = require('cors');
 const graphqlHTTP = require('express-graphql');
 const MyGraphQLSchema = require('./schema/schema');
 require('./utils/pass');
@@ -14,6 +15,7 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- ui/src/components/Hero.tsx
+++ ui/src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Color } from '../Style';
 
@@ -50,42 +50,96 @@ const Styled = {
   `,
 };
 
-export const Hero: React.FC = () => {
-  const linkTypeOptions = [
-    {
-      title: 'as short as possible',
-      description:
-        'The link will be short and sweet. This is the best option when sharing something publicly, like on Twitter.',
-    },
-    {
-      title: 'impossible to quess',
-      description:
-        'The link will be long and extremely hard to guess. Pick this option when privacy is a concern.',
-    },
-    {
-      title: 'secured with a password',
-      description:
-        'The link will be short, but only those with the password can access it.',
-    },
-  ];
+const linkTypeOptions = [
+  {
+    title: 'as short as possible',
+    description:
+      'The link will be short and sweet. This is the best option when sharing something publicly, like on Twitter.',
+  },
+  {
+    title: 'impossible to quess',
+    description:
+      'The link will be long and extremely hard to guess. Pick this option when privacy is a concern.',
+  },
+  {
+    title: 'secured with a password',
+    description:
+      'The link will be short, but only those with the password can access it.',
+  },
+];
 
-  const linkAnalyticsOptions = [
-    {
-      title: 'I want to see',
-      description:
-        'You will have access to an admin page where detailed usage statistics can be viewed.',
+const linkAnalyticsOptions = [
+  {
+    title: 'I want to see',
+    description:
+      'You will have access to an admin page where detailed usage statistics can be viewed.',
+  },
+  {
+    title: 'I dont want to see',
+    description:
+      'The amount of clicks will be visible, but no detailed statistics will be recorded.',
+  },
+  {
+    title: 'I dont want anyone to see',
+    description:
+      'Nothing will be recorded from this link. This option requires the link to have a deadline.',
+  },
+];
+
+const apiURL = 'http://localhost:3001/graphql';
+const shortenedBaseUrl = 'http://localhost:3001/';
+
+const fetchGraphql = async (query: any) => {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      // Authorization: 'Bearer ' + localStorage.getItem('token'),
     },
-    {
-      title: 'I dont want to see',
-      description:
-        'The amount of clicks will be visible, but no detailed statistics will be recorded.',
-    },
-    {
-      title: 'I dont want anyone to see',
-      description:
-        'Nothing will be recorded from this link. This option requires the link to have a deadline.',
-    },
-  ];
+    body: JSON.stringify(query),
+  };
+  try {
+    const response = await fetch(apiURL, options);
+    const json = await response.json();
+    return json.data;
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+};
+
+export const Hero: React.FC = () => {
+  const [isShortened, setIsShortened] = useState<boolean>(false);
+  const [url, setUrl] = useState<string>('');
+
+  const handleUrlChange = (event: any) => {
+    setUrl(event.target.value);
+  };
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+
+    const result = await fetchGraphql({
+      query: `
+        mutation Shorten($url: String!) {
+          shortenUrl(url: $url) {
+            hash
+          }
+        }
+      `,
+      variables: {
+        url,
+      },
+    });
+
+    if (result && result.shortenUrl && result.shortenUrl.hash) {
+      setUrl(`${shortenedBaseUrl}${result.shortenUrl.hash}`);
+      setIsShortened(true);
+    } else {
+      setUrl('');
+    }
+  };
 
   return (
     <Styled.Hero>
@@ -107,9 +161,17 @@ export const Hero: React.FC = () => {
         </ul>
       </div>
       <div className="side">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h3>Just paste any link here and see the magic happen</h3>
-          <input type="text" placeholder="https://" />
+          <input
+            type="text"
+            placeholder="https://"
+            value={url}
+            onChange={handleUrlChange}
+            disabled={isShortened}
+          />
+
+          {!isShortened && <button type="submit">Shorten</button>}
 
           <p>
             I want my link to be{' '}
